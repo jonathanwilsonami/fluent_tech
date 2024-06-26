457f8c812adec582f1ea85cc0f6529b932005f28
Jonathan Wilson <jonwilami@gmail.com>
2024-06-26 16:49:03 -0400

    Update basics.py

--- python/fundamentals/basics.py
+++ python/fundamentals/basics.py
@@ -4,20 +4,7 @@
 >3d
 0.2f
 
-##### Dictionaries #####
-# Questions
-"""
-How can you merge two dictionaries such that the values of overlapping keys are combined into a list?
-How can you merge two dictionaries and sum the values of overlapping keys?
-Write a function to merge multiple dictionaries where the values of overlapping keys are concatenated strings.
-How do you merge dictionaries stored in a list and handle overlapping keys by storing values in a set to avoid duplicates?
-How can you merge dictionaries and create a new dictionary where the keys are the same and the values are lists of all values associated with those keys in the original dictionaries?
-How would you merge two dictionaries by recursively merging nested dictionaries and summing the values of overlapping keys?
-Write a function that merges a list of dictionaries where the keys are the same, and the values are combined into a list of unique values.
-How can you merge dictionaries with keys representing dates and aggregate their values, assuming the values are numerical?
-Write a function to merge dictionaries where the values are sets of elements, ensuring the merged dictionary has combined sets without duplicates.
-How can you merge dictionaries by concatenating the string values of overlapping keys and retaining unique values for non-overlapping keys?
-"""
+
 
 #### System #####
 # Questions 
