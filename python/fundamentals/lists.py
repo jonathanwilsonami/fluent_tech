66a233c236cbfacdde55721f06fcb31d84e13aaa
Jonathan Wilson <jonwilami@gmail.com>
2024-06-26 16:47:41 -0400

    Create lists.py

---
+++ python/fundamentals/lists.py
@@ -0,0 +1,14 @@
+##### Lists #####
+# Questions
+"""
+How do you create an empty list in Python?
+How can you add an element to the end of a list?
+How do you access the first element of a list?
+How can you remove an element from a list by its value?
+What is the difference between append() and extend() methods?
+How do you find the length of a list?
+Write a Python script to create a list of numbers from 1 to 10.
+How can you sort a list in ascending order?
+How do you reverse the elements of a list?
+Write a Python script to find the largest number in a list of integers.
+"""
