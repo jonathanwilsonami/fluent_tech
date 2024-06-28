2830b835f397623d4799b9acaf1aae0c39778761
Jonathan Wilson <jonwilami@gmail.com>
2024-06-28 07:03:20 -0400

    Update strings.py

--- python/fundamentals/strings.py
+++ python/fundamentals/strings.py
@@ -4,7 +4,7 @@ s = "some string/"
 s.strip()
 s.strip('/')
 str(s) # uses the \n
-repr(s) # prints the newline chat \n. This is nice for debugging
+repr(s) # This is nice for debugging
 
 ##### Format specifier #####
 num = 5
@@ -46,4 +46,32 @@ print(f"'{formatted_str_lower}', '{formatted_str_upper}'")
 
 # Questions 
 """
+Reverse a String: Write a function to reverse a string.
+Check Palindrome: Write a function to check if a string is a palindrome.
+Count Vowels: Write a function to count the number of vowels in a string.
+Remove Spaces: Write a function to remove all spaces from a string.
+Check Anagram: Write a function to check if two strings are anagrams.
+
+Longest Substring Without Repeating Characters: Write a function to find the length of the longest substring without repeating characters.
+String to Integer: Write a function to convert a string to an integer.
+Find Duplicate Characters: Write a function to find all duplicate characters in a string.
+String Compression: Write a function to perform basic string compression using the counts of repeated characters.
+Longest Common Prefix: Write a function to find the longest common prefix among a list of strings.
+
+KMP Algorithm: Implement the KMP algorithm for pattern searching in a string.
+Minimum Window Substring: Write a function to find the minimum window substring which contains all characters of another string.
+Longest Palindromic Substring: Write a function to find the longest palindromic substring.
+Group Anagrams: Write a function to group anagrams from a list of strings.
+Z Algorithm: Implement the Z algorithm for pattern searching in a string.
+Edit Distance: Write a function to calculate the edit distance between two strings.
+Rabin-Karp Algorithm: Implement the Rabin-Karp algorithm for pattern searching in a string.
+Word Break: Write a function to determine if a string can be segmented into a space-separated sequence of one or more dictionary words.
+Permutations of a String: Write a function to generate all permutations of a given string.
+Rotate String: Write a function to determine if one string is a rotation of another.
 """
+# Count the number of times 'a' shows up in a string 
+from collections import Counter 
+word = "apples hcjaa aplha techno james"
+g = Counter(words)
+print(g.get('a'))
+
